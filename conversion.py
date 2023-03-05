8241b37908be6bd041a8a5dc221836084cbfa97a
fredmyricks <82068306+fredmyricks@users.noreply.github.com>
2023-03-05 17:57:54 -0500

    Update conversion.py

--- conversion.py
+++ conversion.py
@@ -1,17 +1,57 @@
 def convertCelsiusToKelvin(celsius):
-    return 0.0
+    """
+    Convert Celsius to Kelvin
+    celsius + 273.5
+    :param celsius:
+    :return:
+    """
+    return celsius + 273.15
+
 
 def convertCelsiusToFahrenheit(celsius):
-    return 0.0
+    """
+    Convert Celsius to Fahrenheit
+    9/5 * celsius + 32
+    :param celsius:
+    :return:
+    """
+    return (celsius * 9/5) + 32
+
+# TODO: add 2 more functions to convert F to C and F to K
 
 def convertFahrenheitToCelsius(fahrenheit):
-    return 0.0
+    """
+    Convert Fahrenheit to Celsius
+    (Fahrenheit – 32) * 5/9
+    :param fahrenheit:
+    :return:
+    """
+    return (fahrenheit - 32) * 5/9
 
 def convertFahrenheitToKelvin(fahrenheit):
-    return 0.0
+    """
+    Convert Fahrenheit to Kelvin
+    (fahrenheit + 459.67) * 5/9
+    :param fahrenheit:
+    :return:
+    """
+    return (fahrenheit + 459.67) * 5/9
 
-def convertKelvinToCelsius(kelvin):
-    return 0.0
+def convertKelvintoFahrenheit(kelvin):
+    """
+    Convert Kelvin to Fahrenheit
+    (Fahrenheit * 9/5 − 459.67)
+    :param kelvin:
+    :return:
+    """
+    return (kelvin * 9/5) - 459.67
 
-def convertKelvinToFahrenheit(kelvin):
-    return 0.0
+
+def convertKelvintoCelsius(kelvin):
+    """
+    Convert Kelvin to Celsius
+    Kelvin - 273.15
+    :param kelvin:
+    :return:
+    """
+    return kelvin - 273.15
